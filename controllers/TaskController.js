cabf45d0d4622596bb990644894874c4756e2a7b
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-22 00:21:26 +0200

    update task endpoint ready and working

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -67,36 +67,40 @@ const TaskController = {
     }
   },
 
-  //   async completedTask(req, res) {
-  //     try {
-  //       const { id } = req.params;
+  async updateTask(req, res) {
+    try {
+      const { id } = req.params;
+      const { title } = req.body;
 
-  //       if (!ObjectId.isValid(id)) {
-  //         return res.status(400).json({ message: "Invalid Task ID format" });
-  //       }
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).json({
+          message: "Invalid Task ID format",
+        });
+      }
 
-  //       const task = await Task.findByIdAndUpdate(
-  //         id,
-  //         { completed: true },
-  //         { new: true }
-  //       );
+      if (!title || typeof title !== "string" || title.trim() === "") {
+        return res.status(400).json({
+          message: "`title`is required and must be a non-empty string",
+        });
+      }
 
-  //       if (!task) {
-  //         return res.status(404).json({ message: "Task not found" });
-  //       }
+      const taskUpdated = await Task.findByIdAndUpdate(
+        id,
+        { title: title.trim() },
+        { new: true }
+      );
 
-  //       res.status(200).json({
-  //         message: "Task completed",
-  //         task,
-  //       });
-  //     } catch (error) {
-  //       console.error(error);
-  //       res.status(500).json({
-  //         message: "Server error while marking task as completed",
-  //         error: error.message,
-  //       });
-  //     }
-  //   },
+      res.status(200).json({
+        message: "Task updated",
+        taskUpdated,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: "Server error while updating the task",
+      });
+    }
+  },
 
   async setTaskCompleted(req, res) {
     try {
--- routes/tasks.js
+++ routes/tasks.js
@@ -5,6 +5,7 @@ const TaskController = require("../controllers/TaskController");
 router.post("/create", TaskController.createTask);
 router.get("/", TaskController.getAllTask);
 router.get("/id/:id", TaskController.getTaskById);
+router.put("/:id", TaskController.updateTask);
 router.put("/:id/completed", TaskController.setTaskCompleted);
 
 module.exports = router;
